50468baff5ad0ad251203e8e944f48a95b050518
Jung-uk Choi <choijunguk@gmail.com>
2015-10-27 22:31:53 +0900

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 INSTALL_DIR=`pwd`
-sudo apt-get install build-essential gfortran libatlas-base-dev python-pip python-dev git -y
+sudo apt-get install build-essential gfortran libatlas-base-dev python-pip python-dev libfreetype6-dev git -y
 sudo pip install --upgrade pip
 sudo rm -rf /tmp/PDMChartMaker
 cd /tmp
