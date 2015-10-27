89d030494943c23ac2d40761ef0f812d02b4d67f
Jung-uk Choi <choijunguk@gmail.com>
2015-10-27 22:29:01 +0900

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-PWD=`pwd`
+INSTALL_DIR=`pwd`
 sudo apt-get install build-essential gfortran libatlas-base-dev python-pip python-dev git -y
 sudo pip install --upgrade pip
 sudo rm -rf /tmp/PDMChartMaker
@@ -8,4 +8,4 @@ cd /tmp
 git clone https://github.com/deokwooj/PDMChartMaker
 cd PDMChartMaker
 sh install-pymodules.sh
-mv /tmp/PDMChartMaker $PWD
+mv /tmp/PDMChartMaker $INSTALL_DIR
